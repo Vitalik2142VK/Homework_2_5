523c6c5de8407e94e5c300ecd586679b8abc2157
Vitalii <lennab@mail.ru>
2023-03-21 23:42:13 +0300

    ДЗ выполнено

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,9 @@ public class Main {
 
     public static boolean latinNumeralsUnderlining(char ch) {
         if ( ((int)ch > 47 && (int)ch < 58) || ((int)ch > 64 && (int)ch < 91) || ((int)ch > 96 && (int)ch < 123) || (int)ch == 95 )
-            return true;
-        else
             return false;
+        else
+            return true;
     }
 
     public static boolean entrance(String login, String password, String confirmPassword) {
@@ -15,9 +15,9 @@ public class Main {
             if (login.length() > 20) {
                 throw new WrongLoginException("Введенный логин более 20ти символов. Введите меньшее колличество символов.");
             }
-            for (int i = 0; i < password.length(); i++) {
+            for (int i = 0; i < login.length(); i++) {
                 if (latinNumeralsUnderlining(login.charAt(i))) {
-                    throw new WrongLoginException("Не допустимые символы. Можно вводить только латинские буквы, цифры и знак подчеркивания. \nНапример, java_skypro");
+                    throw new WrongLoginException("Не допустимые символы в логине. Можно вводить только латинские буквы, цифры и знак подчеркивания. \nНапример, java_skypro");
                 }
             }
 
@@ -25,8 +25,8 @@ public class Main {
                 throw new WrongPasswordException("Введенный пароль более 19ти символов. Введите меньшее колличество символов.");
             }
             for (int i = 0; i < password.length(); i++) {
-                if (latinNumeralsUnderlining(login.charAt(i))) {
-                    throw new WrongPasswordException("Не допустимые символы. Можно вводить только латинские буквы, цифры и знак подчеркивания. \nНапример, D_1hWiKjjP_9");
+                if (latinNumeralsUnderlining(password.charAt(i))) {
+                    throw new WrongPasswordException("Не допустимые символы в пароле. Можно вводить только латинские буквы, цифры и знак подчеркивания. \nНапример, D_1hWiKjjP_9");
                 }
             }
 
@@ -37,22 +37,19 @@ public class Main {
             return true;
 
         } catch (WrongLoginException ex) {
+            System.out.println(ex.getMessage());
             return false;
         } catch (WrongPasswordException ex) {
+            System.out.println(ex.getMessage());
             return false;
         }
     }
 
     public static void main(String[] args) {
-        try {
-            if (entrance("SkyPro", "qazXDE321", "qazXDE321"))
-                System.out.println("Данные введены корректно.");
-        } catch (WrongLoginException ex) {
-            ex.getMessage();
-        } catch (WrongPasswordException ex) {
-            ex.getMessage();
-        }
 
+        if (entrance("SkyPro", "qazXDE321", "qazXDE321")) {
+            System.out.println("Данные введены корректно.");
+        }
 
     }
 }
